863643cf5fefe3d8dd2a053082f010832492fc88
Crowdbotics <team@crowdbotics.com>
2020-07-22 07:43:12 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/welcome_to_idb_inve_19039/MainActivity.java
@@ -0,0 +1,15 @@
+package com.welcome_to_idb_inve_19039;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "welcome_to_idb_inve_19039";
+    }
+}
